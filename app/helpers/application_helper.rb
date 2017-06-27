3737f818686316d668eb63265358648a0796f3b6
SakiTanda <sakitsukuda@gmail.com>
2017-06-27 15:16:36 -0500

    modify meta tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,15 +4,17 @@ module ApplicationHelper
   {
     site: 'Freshness',
     reverse: true,
-    title: 'Freshness',
-    description: "Freshness provides the food preservation methods for you to keep food fresh as long as you can. Let's reduce food waste for global environment by using Freshness!",
-    keywords: 'Freshness',
+    title: 'Index page',
+    description: "The index page of Freshness showing two search functions",
+    keywords: %w[food fresh preservation search web],
     canonical: 'http://192.168.1.200:3000/',
+    icon: image_url('apple.ico'),
+    author: 'https://www.linkedin.com/in/saki-tanda-26b0b8141/',
     og: {
       title: 'Freshness',
       type: 'website',
       url: 'http://192.168.1.200:3000/',
-      image: '',
+      image: image_url('logo.png'),
       site_name: 'Freshness',
       description: "Freshness provides the food preservation methods for you to keep food fresh as long as you can. Let's reduce food waste for global environment by using Freshness!"
     },
