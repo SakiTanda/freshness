eedde9fc8b935ce5017daeb2b67b7e3b60f4e415
SakiTanda <sakitsukuda@gmail.com>
2017-07-09 01:15:47 -0500

    modify fb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,13 +7,13 @@ module ApplicationHelper
     title: 'Index page',
     description: "The index page of Freshness showing two search functions",
     keywords: %w[food fresh preservation search web],
-    canonical: 'http://192.168.1.200:3000/',
+    canonical: 'https://obscure-mesa-98847.herokuapp.com',
     icon: image_url('apple.ico'),
     author: 'https://www.linkedin.com/in/saki-tanda-26b0b8141/',
     og: {
       title: 'Freshness',
       type: 'website',
-      url: 'http://192.168.1.200:3000/',
+      url: 'https://obscure-mesa-98847.herokuapp.com',
       image: image_url('logo.png'),
       site_name: 'Freshness',
       description: "Freshness provides the food preservation methods for you to keep food fresh as long as you can. Let's reduce food waste for global environment by using Freshness!"
